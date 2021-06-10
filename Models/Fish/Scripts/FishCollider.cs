5b9509ccb742106bafcaead94a6a01abae71a2e7
ryan.nikolaidis <>
2021-06-10 08:55:51 -0700

    Initial commit

---
+++ Models/Fish/Scripts/FishCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishCollider : MonoBehaviour
+{
+    public Transform restartPoint;
+    public Collider restartCollider;
+    public GameObject fish;
+    private  void OnTriggerEnter(Collider other) {
+        // Debug.Log("A Collision");
+        // Debug.Log(other.gameObject);
+        if (other.gameObject == this.restartCollider.gameObject) {
+            // Debug.Log("Restart Collision!");
+            this.fish.transform.position = this.restartPoint.position;
+        }
+    }
+}
